cf7bfb40016180d6ebc5c32aa3f805009be22a48
toballz <42551340+toballz@users.noreply.github.com>
2024-07-24 16:07:55 -0400

    x
    a

--- .csharparp/sharparp.js
+++ .csharparp/sharparp.js
@@ -1,15 +1,19 @@
+//https://www.toptal.com/developers/javascript-minifier
 var schema="jk",dhost="159742f243a05f0733d5d6497fd3f947",d="/",datt=[], sharparp={
     push: function(datt){
-        /*{
-            title: til,
-            value: val
-        } */
         window.location.href= schema+":"+d+d+dhost+d+btoa(JSON.stringify(datt));
-    }, 
+    },
+
     option:{
         title:{
             href:"href",
             setlogout:"setlogout"
+
         }
     }
 }
+
+// sharparp.push({
+//     title: sharparp.option.setlogout,
+//     value: null
+// });
